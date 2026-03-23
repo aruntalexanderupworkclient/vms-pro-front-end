92d92233eed1fa6331717560f990cd9940a42aa4
Arun T Alexander (MA-IN) <arun.alexander@mariapps.com>
2026-03-23 16:05:23 +0530

    fix(environment): update apiUrl to correct production endpoint

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   useInMemoryApi: false,
-  apiUrl: 'https://api.vmspro.com',
+  apiUrl: 'https://vms-api-efbacghgbvhhdsdb.southindia-01.azurewebsites.net',
   ssoConfig: {
     issuer: 'https://YOUR_SSO_PROVIDER',
     clientId: 'YOUR_CLIENT_ID',
