f3621ca544d2424ec3e335f6e27ddb603b1b4ff7
aruntalexanderupworkclient@gmail.com <aruntalexanderupworkclient@gmail.com>
2026-03-23 17:21:23 +0530

    fix(environment): update apiUrl and add Google Sign-In configuration

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,10 @@ export const environment = {
   production: true,
   useInMemoryApi: false,
   apiUrl: 'https://vms-api-efbacghgbvhhdsdb.southindia-01.azurewebsites.net',
+  
+  // Google Sign-In
+  googleClientId: '169656025674-akam3hj6lsf9skmbhdkv3l63ugderojt.apps.googleusercontent.com',
+  
   ssoConfig: {
     issuer: 'https://YOUR_SSO_PROVIDER',
     clientId: 'YOUR_CLIENT_ID',
